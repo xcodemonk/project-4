5be796638d9d4b207f9f680ae70bf50bf018930d
xcodemonk <xcodemonk.ios@gmail.com>
2023-05-21 20:06:11 +0530

    first comment

---
+++ Project-4/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Project-4
+//
+//  Created by Yogesh on 21/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project-4/Project_4App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_4App.swift
+//  Project-4
+//
+//  Created by Yogesh on 21/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Project_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
